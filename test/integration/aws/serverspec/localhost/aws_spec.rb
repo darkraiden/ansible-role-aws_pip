3e567090555f950472fe133958c905c14aa4f672
Davide Di Mauro <davidedimauro88@gmail.com>
2017-03-27 15:36:42 +0100

    Initial commit

---
+++ test/integration/aws/serverspec/localhost/aws_spec.rb
@@ -0,0 +1,18 @@
+require 'serverspec'
+set :backend, :exec
+
+describe user('test') do
+    it { should exist }
+end
+
+describe file('/home/test/.aws/credentials') do
+    it { should exist }
+    its(:content) { should match /aws_access_key_id(\s+)?=(\s+)?anAccessKey/ }
+    its(:content) { should match /aws_secret_access_key(\s+)?=(\s+)?aSecretKey/ }
+end
+
+describe file('/home/test/.aws/config') do
+    it { should exist }
+    its(:content) { should match /output(\s+)?=(\s+)?json/ }
+    its(:content) { should match /region(\s+)?=(\s+)?eu-west-1/ }
+end
