4c7448eb0b81c1c02ac495647a1182517523ae90
Davide Di Mauro <davidedimauro88@gmail.com>
2018-06-21 13:43:17 +0100

    Refactor and update role (#3)
    * re-work on pipeline and local test environment
    
    * install sudo where test failed
    
    * run idempotence test
    
    * typo
    
    * can't test idempotence until pip role isn't fixed
    
    * don't check changes for this pre task
    
    * test aws cli for root user
    
    * add specific behaviour for root user
    
    * add aws cli version
    
    * make containers consistent with Travis'
    
    * kitchen break on debian-7, moving on
    
    * update readme

--- test/integration/aws/serverspec/localhost/aws_spec.rb
+++ test/integration/aws/serverspec/localhost/aws_spec.rb
@@ -16,3 +16,15 @@ describe file('/home/test/.aws/config') do
     its(:content) { should match /output(\s+)?=(\s+)?json/ }
     its(:content) { should match /region(\s+)?=(\s+)?eu-west-1/ }
 end
+
+describe file('/root/.aws/credentials') do
+    it { should exist }
+    its(:content) { should match /aws_access_key_id(\s+)?=(\s+)?anAccessKey/ }
+    its(:content) { should match /aws_secret_access_key(\s+)?=(\s+)?aSecretKey/ }
+end
+
+describe file('/root/.aws/config') do
+    it { should exist }
+    its(:content) { should match /output(\s+)?=(\s+)?json/ }
+    its(:content) { should match /region(\s+)?=(\s+)?eu-west-1/ }
+end
